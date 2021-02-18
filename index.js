92a4df6504d81bffbebbf0b8fd2f84b03c27d0ab
tatafox <pavlovetstatyana@gmail.com>
2021-02-18 10:34:15 +0300

    fix: bundle error

--- index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-import Controller from "./scripts/Controller/Controller.js";
-import View from "./scripts/View/View.js";
-import Model from "./scripts/Model/Model.js";
-
-const view = new View();
-const model = new Model();
-const app = new Controller(view, model);
